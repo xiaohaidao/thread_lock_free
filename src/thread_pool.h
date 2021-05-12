be04149a7da264b7739f546e0c9c58866b0eefdd
Bobo Li <boboli@ENG.FLCNC.COM>
2021-05-12 16:59:19 +0800

    thread_pool add work_stealing_queue

--- src/thread_pool.h
+++ src/thread_pool.h
@@ -39,27 +39,92 @@ class thread_pool {
         function_wrapper &operator=(const function_wrapper &) = delete;
     };
 
+    class work_stealing_queue {
+        typedef function_wrapper data_type;
+        std::deque<data_type> the_queue;
+        mutable std::mutex the_mutex;
+    public:
+        work_stealing_queue() {}
+        work_stealing_queue(const work_stealing_queue& other) = delete;
+        work_stealing_queue& operator=(
+                const work_stealing_queue& other) = delete;
+
+        void push(data_type data) {
+            std::lock_guard<std::mutex> lock(the_mutex);
+            the_queue.push_front(std::move(data));
+        }
+
+        bool empty() const {
+            std::lock_guard<std::mutex> lock(the_mutex);
+            return the_queue.empty();
+        }
+
+        bool try_pop(data_type& res) {
+            std::lock_guard<std::mutex> lock(the_mutex);
+            if (the_queue.empty()) {
+                return false;
+            }
+            res = std::move(the_queue.front());
+            the_queue.pop_front();
+            return true;
+        }
+        bool try_steal(data_type& res) {
+            std::lock_guard<std::mutex> lock(the_mutex);
+            if (the_queue.empty()) {
+                return false;
+            }
+            res = std::move(the_queue.back());
+            the_queue.pop_back();
+            return true;
+        }
+    };
+
+    typedef function_wrapper task_type;
+
     std::atomic_bool done;
-    lock_free_queue<function_wrapper> work_queue;
-    typedef std::queue<function_wrapper> local_queue_type;
-    static thread_local std::unique_ptr<local_queue_type> local_work_queue;
+    lock_free_queue<task_type> pool_work_queue;
+    std::vector<std::unique_ptr<work_stealing_queue> > queues;
     std::vector<std::thread> threads;
     //join_threads joiner;
 
-    void work_thread() {
-        local_work_queue.reset(new local_queue_type);
+    static thread_local work_stealing_queue *local_work_queue;
+    static thread_local uint32_t my_index;
+
+    void work_thread(uint32_t index) {
+        my_index = index;
+        local_work_queue = queues[my_index].get();
         while (!done) {
             run_pending_task();
         }
     }
 
+    bool pop_task_from_local_queue(task_type &task) {
+        return local_work_queue && local_work_queue->try_pop(task);
+    }
+    bool pop_task_from_pool_queue(task_type &task) {
+        auto t = pool_work_queue.pop();
+        if (t) {
+            task = *t;
+        }
+        return t;
+    }
+    bool pop_task_from_other_thread_queue(task_type &task) {
+        size_t queues_size = queues.size();
+        for (size_t i = 0; i < queues_size; ++i) {
+            size_t index = (my_index + i + 1) % queues_size;
+            if (queues[index]->try_streal(task)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     void run_pending_task() {
-        function_wrapper task;
-        if (local_work_queue && !local_work_queue->empty()) {
-            task = std::move(local_work_queue->front());
-            local_work_queue->pop();
-            task();
-        } else if (task = work_queue.pop()) {
+        task_type task;
+        if (pop_task_from_local_queue(task) ||
+                pop_task_from_pool_queue(task) ||
+                pop_task_from_other_thread_queue(task)) {
+
             task();
         } else {
             std::this_thread::yield();
@@ -67,11 +132,15 @@ class thread_pool {
     }
 
 public:
-    thread_pool() {
+    thread_pool(): done(false) {
         uint32_t const thread_count = std::thread::hardware_concurrency();
         try {
             for (uint32_t i = 0; i < thread_count; ++i) {
-                threads.push_back(std::thread(&thrad_pool::worker_thread, this));
+                queues.push_bask(std::unique_ptr<work_stealing_queue>(
+                            new work_stealing_queue));
+
+                threads.push_back(std::thread(&thrad_pool::worker_thread,
+                            this, i));
             }
         } catch (...) {
             done = true;
@@ -92,7 +161,7 @@ public:
         if (local_work_queue) {
             local_work_queue->push(std::move(task));
         } else {
-            work_queue.push(std::move(task));
+            pool_work_queue.push(std::move(task));
         }
         return res;
     }
